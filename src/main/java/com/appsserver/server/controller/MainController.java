b9157e916a6d98c1b495350add69780e331bcdbd
Ed <edko@ua.fm>
2019-02-11 13:22:11 +0200

    commit 4

--- src/main/java/com/appsserver/server/controller/MainController.java
+++ src/main/java/com/appsserver/server/controller/MainController.java
@@ -3,6 +3,7 @@ package com.appsserver.server.controller;
 import com.appsserver.server.domain.User;
 import com.appsserver.server.repo.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,6 +16,9 @@ import java.util.HashMap;
 @RequestMapping("/")
 public class MainController {
     private final MessageRepo messageRepo;
+
+    @Value("${spring.profiles.active}")
+    private String profile;
 
     @Autowired
     public MainController(MessageRepo messageRepo) {
@@ -29,7 +33,7 @@ public class MainController {
         data.put("messages", messageRepo.findAll());
 
         model.addAttribute("frontendData", data);
-
+        model.addAttribute("isDevMode", "dev".equals(profile));
         return "index";
     }
 }
